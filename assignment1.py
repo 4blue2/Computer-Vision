782c32f44d42c33979632ce4b7def97de54cc761
Sergi Labadze <78698594+4blue2@users.noreply.github.com>
2022-11-04 14:20:56 +0000

    plot the image

--- assignment1.py
+++ assignment1.py
@@ -4,3 +4,10 @@ import matplotlib.pyplot as plt
 
 image = cv2.imread(r"C:\Users\slaba\OneDrive\Desktop\Y3\COMP-388-ComputerVision\Assignment1\victoria.jpg")
 
+def cv2_imshow(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    plt.imshow(image)
+    plt.axis(False)
+    plt.show()
+
+cv2_imshow(image)
