7ed2225e84854f4a15368d1bbbf6437e0b1a9e73
Sergi Labadze <78698594+4blue2@users.noreply.github.com>
2022-11-05 23:15:31 +0000

    padding implementation done for both rows and columns

--- assignment1.py
+++ assignment1.py
@@ -12,15 +12,44 @@ def cv2_imshow(image):
     plt.axis(False)
     plt.show()
 
+#get height and width of an image
 def get_image_dimensions(image):
     h = image.shape[0]
     w = image.shape[1]
     return(h,w)
 
+#get height and width of the filter
 def get_filter_dimensions(filter):
     h = filter.shape[0]
     w = filter.shape[1]
     return(h,w)
+
+#adding padding to image rows based on the width of the current image
+def rows_padding(image):
+    _,image_width = get_image_dimensions(image)
+    padding = np.zeros((image_width,), dtype=int)
+
+    pad_image_rows = np.insert(image, 0, padding, axis=0)
+
+    pad_image_rows = np.append(pad_image_rows, [padding], axis=0)
+
+    return(pad_image_rows)
+
+def col_padding(image):
+    image_height,_ = get_image_dimensions(image)
+
+    pad_left_side = np.zeros((image_height,), dtype=int)
+    pad_right_side = np.zeros((image_height,1), dtype=int)
+    
+    pad_image_col = np.insert(image, 0, [pad_left_side], axis=1)
+
+    pad_image_col = np.append(pad_image_col, pad_right_side, axis=1)
+
+    return(pad_image_col)
+
+
+#def filter2D(image,filter):
+#gray = np.append(image, pad, axis=0)
 
 image_height, image_width = get_image_dimensions(image)
 
